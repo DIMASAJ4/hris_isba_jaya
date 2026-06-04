1488caf2103a5c997d8d1c30aa861a1b28a10745
Dimas Anggoro Hrp <dima21087si@student.nurulfikri.ac.id>
2026-06-04 23:04:28 +0700

    fix: implement robust image onerror fallback for sidebar and login logos

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,5 @@
-<img src="/images/logoasli.png" {{ $attributes->merge(['class' => 'h-12 w-auto object-contain']) }} alt="Logo ISBA JAYA">
+<img src="/images/logoasli.png" 
+     onerror="if(this.src.indexOf('/public/') === -1){ this.src = '/public/images/logoasli.png'; } else { this.src = '/images/logo.png'; }" 
+     {{ $attributes->merge(['class' => 'h-12 w-auto object-contain']) }} 
+     alt="Logo ISBA JAYA">
 
